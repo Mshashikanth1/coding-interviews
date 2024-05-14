7be48b3e6140614dfc4a1a6fe0e2c1de5eb2526e
shashikanth <mallepally.varma@jsw.in>
2024-05-14 20:49:20 +0530

    interview question sync

---
+++ src/technoHertz.java
@@ -0,0 +1,38 @@
+public class technoHertz {
+
+    /*
+    * [Ashay Chavan : 14/tue/2024]
+    * SQL QUERY to : third highest salary from table
+    *
+    *    emp table : "<f.name> <s.name >  <sale>
+
+    * 1
+    * 2
+    * 3
+    *
+    * 3,2 1
+
+    *
+    * select * from (
+    *  select * from
+    *      (select * from  emp_table order sal desc limit 3  ) as main  order by sal asc) as main2
+    *   limit 1
+    *
+    *   ---<top>---
+    *
+    * what is Garbage collector ?
+    * what is buffer stream/ input stream ??
+    *
+    * <Angular questions?
+    * what is directives?
+    * what is component?
+    * what is REST API?
+    * what is interpolation?
+    * what is data binding?
+    * what is two-way-data binding?
+    * what is service?
+    * what is different between directives and components?
+     * */
+}
+
+
